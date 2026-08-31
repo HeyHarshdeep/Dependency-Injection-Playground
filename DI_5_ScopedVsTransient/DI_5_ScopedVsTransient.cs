e1d2314dba553008de4732a5998a8f1c66befdf4
HeyHarshdeep <harshdeeps1313@gmail.com>
2026-09-01 01:01:01 +0530

    scoped vs transient

---
+++ DI_5_ScopedVsTransient/DI_5_ScopedVsTransient.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+IServiceCollection collection = new ServiceCollection();
+
+collection.AddScoped<Scoped>();
+collection.AddTransient<Transient>();
+
+var service = collection.BuildServiceProvider();
+Parallel.For(1, 10, i =>
+{
+
+    var scopedObject = service.GetRequiredService<Scoped>();
+    var transientObject = service.GetRequiredService<Transient>();
+
+    Console.WriteLine($"Scoped Id:  {scopedObject.GetHashCode()}");
+    Console.WriteLine($"Transient Id:  {transientObject.GetHashCode()}");
+
+});
+
+Console.WriteLine("----------");
+
+for (int j= 0; j < 10; j++){
+
+
+
+    var scopedObject = service.GetRequiredService<Scoped>();
+    var transientObject = service.GetRequiredService<Transient>();
+
+    Console.WriteLine($"Scoped Id:  {scopedObject.GetHashCode()}");
+    Console.WriteLine($"Transient Id:  {transientObject.GetHashCode()}");
+
+}
+Console.ReadKey();
+
+public class Scoped()
+{
+
+}
+
+public class Transient()
+{
+
+}
