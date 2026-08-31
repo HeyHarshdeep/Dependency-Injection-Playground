c3a9624b7be3147e77c8ae0895c416ae65aaa2ea
HeyHarshdeep <harshdeeps1313@gmail.com>
2026-08-31 23:07:14 +0530

    Add project files.

---
+++ Basic-DI/Program.cs
@@ -0,0 +1,51 @@
+﻿UserInterface UserInterface = new UserInterface();
+UserInterface.GetData();
+public class UserInterface
+{
+    private string _username;
+    private string _password;
+
+    public void GetData()
+    {
+        Console.WriteLine("Enter Username:");
+        _username = Console.ReadLine();
+
+        Console.WriteLine("Enter Password");
+        _password = Console.ReadLine();
+
+        Console.ReadKey();
+    }
+
+    public void SignUp()
+    {
+        GetData();
+
+        var biz = new Business();
+        biz.SignUp(_username, _password);
+    }
+}
+
+public class Business
+{
+    public void SignUp(string userName, string passWord)
+    {
+        var da = new DataAccessSql();
+        da.SignUp(userName, passWord);
+    }
+
+}
+
+public class DataAccessSql
+{
+    public void SignUp(string userName, string passWord)
+    {
+        Console.Write($"Suppose here. Data has been saved to DB with username {userName} password {passWord}");
+    }
+}
+public class DataAccessMySql
+{
+    public void SignUp(string userName, string passWord)
+    {
+
+    }
+}
